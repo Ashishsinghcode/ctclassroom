0cbd2b8d2de2785acd876b24fead6867e9d3f631
Ashish <Ashish9471847@gmail.com>
2022-11-30 21:32:11 +0530

    30/11/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursepageComponent } from './coursepage/coursepage.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AdminloginComponent } from './login/adminlogin/adminlogin.component';
+import { StudentloginComponent } from './login/studentlogin/studentlogin.component';
+import { TeacherloginComponent } from './login/teacherlogin/teacherlogin.component';
 
 const routes: Routes = [
   {
@@ -12,9 +19,33 @@ const routes: Routes = [
     children:[
       {
         path:'dashboard',component:DashboardComponent
+      },
+      {
+        path:'contact',component:ContactComponent
+      },
+      {
+        path:'coursepage',component:CoursepageComponent
       }
     ]
+  },
+  {
+    path:'admin',component:AdminComponent,
+    children:[
+      {
+        path:'admin-dashboard',component:AdminDashboardComponent
+      }
+    ]
+  },
+  {
+    path:'adminlogin',component:AdminloginComponent
+  },
+  {
+    path:'teacherlogin',component:TeacherloginComponent
+  },
+  {
+    path:'studentlogin',component:StudentloginComponent
   }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursepageComponent } from './coursepage/coursepage.component';
+import { AdminloginComponent } from './login/adminlogin/adminlogin.component';
+import { StudentloginComponent } from './login/studentlogin/studentlogin.component';
+import { TeacherloginComponent } from './login/teacherlogin/teacherlogin.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,15 @@ import { LoginComponent } from './login/login.component';
     HeaderComponent,
     FooterComponent,
     DashboardComponent,
-    LoginComponent
+    ContactComponent,
+    CoursepageComponent,
+    AdminloginComponent,
+    StudentloginComponent,
+    TeacherloginComponent,
+    AdminComponent,
+    AdminDashboardComponent,
+   
+  
   ],
   imports: [
     BrowserModule,
